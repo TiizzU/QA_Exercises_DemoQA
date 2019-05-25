780ba6b0a564833aebf53578ac18fbc52729f484
Valentin Ivanov <Eddie_v@abv.bg>
2019-05-25 22:54:28 +0300

    SetUp

--- QA_Exercises_DemoQA/BaseTest.cs
+++ QA_Exercises_DemoQA/BaseTest.cs
@@ -1,9 +1,50 @@
 ﻿
 namespace QA_Exercises_DemoQA
 {
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Remote;
     using System;
 
+    [TestFixture]
     public class BaseTest
     {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Driver
+            driver = GetChromeDriver();
+
+            // Maximize The Window
+            driver.Manage().Window.Maximize();
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        private RemoteWebDriver GetChromeDriver()
+        {
+            var path = Environment.GetEnvironmentVariable("ChromeWebDriver", EnvironmentVariableTarget.Machine);
+            var options = new ChromeOptions();
+            options.AddArgument("--no-sandbox");
+
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                return new ChromeDriver(path, options, TimeSpan.FromSeconds(30));
+            }
+            else
+            {
+                return new ChromeDriver(options);
+            }
+        }
+
     }
+
 }
